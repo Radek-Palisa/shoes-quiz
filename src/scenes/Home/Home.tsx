7dbdb64d07b712a668438fbdb03d5d067e939885
Radek Palisa <radek.palisa@gmail.com>
2022-02-24 21:12:15 +0100

    link to the quiz page

--- src/scenes/Home/Home.tsx
+++ src/scenes/Home/Home.tsx
@@ -1,7 +1,12 @@
+import { Routes, useRouter } from '../../components/Router';
+
 export default function Home() {
+  const [, setRoute] = useRouter();
+
   return (
     <main>
       <h1>Home</h1>
+      <button onClick={() => setRoute(Routes.Quiz)}>Try on trial</button>
     </main>
   );
 }
