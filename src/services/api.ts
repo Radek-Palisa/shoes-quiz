ec5ef0dc92287dc976f48f07f348391918c04e46
Radek Palisa <radek.palisa@gmail.com>
2022-02-25 20:08:52 +0100

    handle fetch request status not ok

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,16 +3,25 @@ import { sleep } from './utils/sleep';
 
 const API_ENDPOINT = '/api/data.json';
 
+function fetch<T>(requestInfo: RequestInfo): Promise<T> {
+  return window.fetch(requestInfo).then((response) => {
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    return response.json();
+  });
+}
+
 export async function fetchQuizQuestions(): Promise<QuizQuestion[]> {
   await sleep(200);
-  return fetch(API_ENDPOINT)
-    .then((response) => response.json())
-    .then((res) => res.questions);
+  const { questions } = await fetch<{ questions: QuizQuestion[] }>(
+    API_ENDPOINT
+  );
+  return questions;
 }
 
 export async function fetchQuizShoes(): Promise<QuizShoe[]> {
   await sleep(400);
-  return fetch(API_ENDPOINT)
-    .then((response) => response.json())
-    .then((res) => res.shoes);
+  const { shoes } = await fetch<{ shoes: QuizShoe[] }>(API_ENDPOINT);
+  return shoes;
 }
