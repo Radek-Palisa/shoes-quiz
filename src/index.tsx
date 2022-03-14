67b4a9b304ca3825cb9c8733faa0b6436516ba68
Radek Palisa <radek.palisa@gmail.com>
2022-03-14 17:20:00 +0100

    add css theme & globals

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import './index.css';
 import App from './App';
 import Router from './components/Router';
 import ErrorBoundary from './components/ErrorBoundary';
+
+import './theme/globals.module.css';
 
 const queryClient = new QueryClient({
   defaultOptions: {
