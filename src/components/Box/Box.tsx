e119c82e3d385ca9c608f58aa49ca427a01a9ed3
Radek Palisa <radek.palisa@gmail.com>
2022-03-14 22:49:20 +0100

    extend Box with new props

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -7,27 +7,33 @@ type Props = {
   children: ReactNode;
   direction?: 'row' | 'column';
   alignItems?: 'start' | 'center';
+  justifyContent?: 'start' | 'center';
   gap?: Space;
   padding?: Partial<{
     inline: Space;
     block: Space;
   }>;
+  height?: 'auto' | 'full';
 };
 
 export default function Box({
   children,
   direction = 'column',
   alignItems = 'start',
+  justifyContent = 'start',
   gap = 'spaceMd',
   padding,
+  height = 'auto',
 }: Props) {
   const className = [
     styles.root,
     styles[`direction-${direction}`],
     styles[`align-items-${alignItems}`],
+    styles[`justify-content-${justifyContent}`],
     styles[`gap-${gap}`],
     styles[`padding-inline-${padding?.inline ?? 'none'}`],
     styles[`padding-block-${padding?.block ?? 'none'}`],
+    styles[`height-${height}`],
   ].join(' ');
 
   return <div className={className}>{children}</div>;
