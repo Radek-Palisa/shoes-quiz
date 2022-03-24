e1cbcabbb0d34f97602bae96398d7b498f2ced2d
Radek Palisa <radek.palisa@gmail.com>
2022-03-24 23:04:55 +0100

    move assets to public folder

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,5 +1,10 @@
-import loadingGif from '../../static/assets/loader.gif';
-
 export default function Loading() {
-  return <img width={50} height={50} src={loadingGif} alt="loading" />;
+  return (
+    <img
+      width={50}
+      height={50}
+      src={`${process.env.PUBLIC_URL}/assets/loader.gif`}
+      alt="loading"
+    />
+  );
 }
