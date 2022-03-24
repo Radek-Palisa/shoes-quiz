04ddffcb5d01442f793e1d2bd6cf270daf7f08f3
Radek Palisa <radek.palisa@gmail.com>
2022-03-24 23:05:59 +0100

    map shoe thumbnails to shoes ids

--- src/services/queries/quizData.ts
+++ src/services/queries/quizData.ts
@@ -3,6 +3,17 @@ import { fetchQuizData } from '../api';
 
 const QUERY_KEY = 'quizData';
 
+const shoeIdToImageUrl: Record<string, string> = {
+  cloud: '/assets/Cloud.png',
+  cloudx: '/assets/Cloud X.png',
+  cloudflow: '/assets/Cloudflow.png',
+  cloudventure: '/assets/Cloudventure.png',
+  cloudsurfer: '/assets/Cloudsurfer.png',
+  cloudventure_waterproof: '/assets/Cloudventure Waterproof.png',
+  cloudventure_peak: '/assets/Cloudventure Peak.png',
+  cloudflyer: '/assets/Cloudflyer.png',
+};
+
 export function useQuizData() {
   const { data } = useQuery(QUERY_KEY, fetchQuizData, {
     staleTime: 600_000,
@@ -12,7 +23,17 @@ export function useQuizData() {
   // data are guaranteed to be defined because of the suspense
   const { shoes, questions } = data!;
 
-  const shoesMap = new Map(shoes.map(({ id, ...shoe }) => [id, shoe]));
+  const shoesMap = new Map(
+    shoes.map(({ id, ...shoe }) => {
+      return [
+        id,
+        {
+          ...shoe,
+          imageUrl: shoeIdToImageUrl[id],
+        },
+      ];
+    })
+  );
 
   return {
     shoes: shoesMap,
