3d95f3c2fafd90be1b4c90c332579f8cea9623bb
Radek Palisa <radek.palisa@gmail.com>
2022-03-26 17:40:53 +0100

    quizResult query: add comment

--- src/services/queries/quizResult.ts
+++ src/services/queries/quizResult.ts
@@ -3,6 +3,7 @@ import { sleep } from '../utils/sleep';
 export async function getQuizResult(rating: [string, number][]) {
   await sleep(2000);
 
+  // Assuming this logic would be on the server and we would fetch it.
   const sortedShoeIds = rating
     .sort((a, b) => b[1] - a[1])
     .map(([shoeId]) => shoeId);
