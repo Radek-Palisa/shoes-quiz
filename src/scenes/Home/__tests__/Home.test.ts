1fd902bd074309fabf1234cebeccbbcc0aab1b44
Radek Palisa <radek.palisa@gmail.com>
2022-03-27 10:32:23 +0200

    add test scenarios

---
+++ src/scenes/Home/__tests__/Home.test.ts
@@ -0,0 +1,7 @@
+describe('Home', () => {
+  it.todo('hovering over the "try quiz" button prefetches the quiz data');
+  it.todo('clicking on the "try quiz" routes to the quiz');
+});
+
+// eslint-disable-next-line jest/no-export
+export {};
---
+++ src/scenes/Quiz/__tests__/Quiz.test.ts
@@ -0,0 +1,18 @@
+describe('Quiz', () => {
+  it.todo('shows loading when data is being fetched');
+  it.todo('shows an error screen when data fetching failed');
+  it.todo('displays only one question at a time');
+  it.todo('uses semantic html');
+  it.todo('clicking an answer transitions to the next question');
+  it.todo(
+    'renders the answer button as type "submit" when being on the last question'
+  );
+  it.todo(
+    'answering the final question fetches the quiz results and shows the results screen'
+  );
+  it.todo('displays a product card for each result');
+  it.todo('clicking on the "restart quiz" button restarts the quiz');
+});
+
+// eslint-disable-next-line jest/no-export
+export {};
