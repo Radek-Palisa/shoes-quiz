99c6d6714bfa1e9050c4b264766c70e98010455d
Radek Palisa <radek.palisa@gmail.com>
2022-03-27 10:47:28 +0200

    update comment

--- src/services/queries/quizResult.ts
+++ src/services/queries/quizResult.ts
@@ -3,7 +3,7 @@ import { sleep } from '../utils/sleep';
 export async function getQuizResult(rating: Record<string, number>) {
   await sleep(2000);
 
-  // Assuming this logic would be on the server and we would fetch it.
+  // Assuming this logic would be on the server and we would need to fetch it.
   const sortedShoeIds = Object.entries(rating)
     .sort((a, b) => b[1] - a[1])
     .map(([shoeId]) => shoeId);
